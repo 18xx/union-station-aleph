489be670c5cca96ca6642b648d6ebe78711e896b
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-12 01:18:21 +0000

    Basic menu

--- app.tsx
+++ app.tsx
@@ -6,11 +6,18 @@ export default function App(
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
 ) {
   return (
-    <main>
+    <>
       <head>
         <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" href="style/index.css" />
       </head>
-      <Page {...pageProps} />
-    </main>
+      <header>
+        <img src="/menu.svg" width="24" height="24" title="Menu" />
+        <h6>Union Station</h6>
+      </header>
+      <main>
+        <Page {...pageProps} />
+      </main>
+    </>
   );
 }
