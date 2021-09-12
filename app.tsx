17ab795d375c35d8adb83bc92d90dcada9d88265
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-12 11:49:13 +0000

    Material-UI Header

--- app.tsx
+++ app.tsx
@@ -1,6 +1,7 @@
 // deno-lint-ignore-file no-explicit-any
 
 import React, { ComponentType } from "react";
+import Header from "~/components/header.tsx";
 
 export default function App(
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
@@ -11,10 +12,7 @@ export default function App(
         <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="style/index.css" />
       </head>
-      <header>
-        <img src="/menu.svg" width="24" height="24" title="Menu" />
-        <h6>Union Station</h6>
-      </header>
+      <Header siteTitle="Union Station" />
       <main>
         <Page {...pageProps} />
       </main>
